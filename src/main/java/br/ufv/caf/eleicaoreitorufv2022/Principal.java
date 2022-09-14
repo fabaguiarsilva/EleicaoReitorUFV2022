bc4b69a075619c7bc094c270211ee8ef21e1a8cf
Fabrício <fabricio.silva@cinnecta.com>
2022-09-14 15:25:53 -0300

    Commit inicial

---
+++ src/main/java/br/ufv/caf/eleicaoreitorufv2022/Principal.java
@@ -0,0 +1,13 @@
+package br.ufv.caf.eleicaoreitorufv2022;
+
+/**
+ *
+ * @author fabriciosilva
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
